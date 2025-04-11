144351c8a03fa6369d38ee21ac8dcd9b57e3ce2d
karthi9772 <karthi.g9772@gmail.com>
2025-04-11 17:20:23 +0530

    Add: Entity

---
+++ src/main/java/dev/karthi/gym_project/entity/Member.java
@@ -0,0 +1,133 @@
+package dev.karthi.gym_project.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Member {
+    private String memberId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String gender;
+    private LocalDate dateOfBirth;
+    private String address;
+    private String city;
+    private String country;
+    private String postalCode;
+    private boolean isActive;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    
+    
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public Member(String memberId, String firstName, String lastName, String email, String phoneNumber, String gender,
+			LocalDate dateOfBirth, String address, String city, String country, String postalCode, boolean isActive,
+			LocalDateTime createdAt, LocalDateTime updatedAt) {
+		super();
+		this.memberId = memberId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.gender = gender;
+		this.dateOfBirth = dateOfBirth;
+		this.address = address;
+		this.city = city;
+		this.country = country;
+		this.postalCode = postalCode;
+		this.isActive = isActive;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+	
+	public Member() {
+		super();
+	}
+
+    
+    
+}
---
+++ src/main/java/dev/karthi/gym_project/entity/Subscription.java
@@ -0,0 +1,74 @@
+package dev.karthi.gym_project.entity;
+
+import jakarta.persistence.Entity;
+
+
+import java.time.LocalDateTime;
+
+public class Subscription {
+    private Long id;
+    private String memberId; // foreign key to Member
+    private String planType; // monthly, 3months, etc.
+    private double amount;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private boolean isActive;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getPlanType() {
+		return planType;
+	}
+	public void setPlanType(String planType) {
+		this.planType = planType;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public LocalDateTime getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(LocalDateTime startDate) {
+		this.startDate = startDate;
+	}
+	public LocalDateTime getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(LocalDateTime endDate) {
+		this.endDate = endDate;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public Subscription(Long id, String memberId, String planType, double amount, LocalDateTime startDate,
+			LocalDateTime endDate, boolean isActive) {
+		super();
+		this.id = id;
+		this.memberId = memberId;
+		this.planType = planType;
+		this.amount = amount;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.isActive = isActive;
+	}
+	public Subscription() {
+		super();
+	}
+
+   
+}
