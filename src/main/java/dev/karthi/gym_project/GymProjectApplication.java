3bbe5e239c46aadd414bd57c9d4d01ef1532bb3c
karthi9772 <karthi.g9772@gmail.com>
2025-04-11 16:57:14 +0530

    Initial

---
+++ src/main/java/dev/karthi/gym_project/GymProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.karthi.gym_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/karthi/gym_project/GymProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.karthi.gym_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
